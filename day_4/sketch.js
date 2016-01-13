fe01a0681691d4292e32555c9e26309b55e95209
ShihuiSandoval <dollxun@gmail.com>
2016-01-13 21:35:32 +0800

    day 4
    demo

---
+++ day_4/sketch.js
@@ -0,0 +1,53 @@
+var xt = 500;
+var c = 0;
+var i = 0;
+var speed = 1.5;
+var touch = false;
+var touchTime = 0;
+
+function setup() {
+  createCanvas(window.innerWidth,window.innerHeight);
+  background(0);
+}
+
+function draw() {
+  if(touch){
+  colorMode(HSB);
+ stroke(c%255,68,100,0.5);
+ strokeWeight(3);
+  line(mouseX,mouseY,i,height);
+  if(i>width){
+    speed = -1.5;
+  }
+  i = i + speed;
+  
+  if(i<0){
+    speed = 1.5;
+  }
+  i = i + speed;
+  
+  xt += 10;
+  fill(c%255,68,100);
+  ellipse(mouseX,mouseY,5,5);
+  c+=1;
+ }
+}
+
+function reset(){
+  clear();
+    background(0);
+    i = 0;
+    touch = false;
+}
+function touchStarted(){
+  if(millis() - touchTime < 300){
+    //double touches reset
+    reset();
+  }
+  else{
+    touchTime = millis();
+  }
+}
+function touchMoved(){
+  touch = true; 
+}
